7414b5f4c7a2586ebfafd9cfab892b70445a9ae0
George Hammond <george.hammond@amalitech.org>
2022-04-13 14:55:43 +0000

    Initial Commit-Trivia Card

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(() =>{
+    //Question Show On Click
+  $('.hint-box').on('click', () =>{
+    $('.hint').slideToggle(500)
+  })
+  
+  //Wrong answers fade out
+  $('.wrong-answer-one').on('click', () =>{
+    $('.wrong-text-one').fadeOut('slow')
+  });
+  $('.wrong-answer-two').on('click', () =>{
+    $('.wrong-text-two').fadeOut('slow')
+  });
+  $('.wrong-answer-three').on('click', () =>{
+    $('.wrong-text-three').fadeOut('slow')
+  });
+  
+  //Wrong answers show frown face
+  $('.wrong-answer-one').on('click', () =>{
+    $('.frown').show()
+  });
+  $('.wrong-answer-two').on('click', () =>{
+    $('.frown').show()
+  });
+  $('.wrong-answer-three').on('click', () =>{
+    $('.frown').show();
+  })
+  
+  //Correct Answer 
+  $('.correct-answer').on('click', ()=> {
+    $('.frown').hide();
+    $('.smiley').show();
+    $('.wrong-text-one').fadeOut('slow')
+    $('.wrong-text-two').fadeOut('slow')
+    $('.wrong-text-three').fadeOut('slow')
+  })
+  });
